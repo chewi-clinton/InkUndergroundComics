1ccc79fc9266257fe7cd38dc7c1716061412514b
chewi-clinton <suhchenwi48@gmail.com>
2025-11-25 07:27:02 +0100

    feat: enhance Header component with social media icons and responsive design

--- InkUndergroundComics/src/components/Header.jsx
+++ InkUndergroundComics/src/components/Header.jsx
@@ -1,51 +1,133 @@
 // Header.jsx
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Header.css";
+import logo from "../assets/logo.png";
+import facebook from "../assets/facebook.jpg";
+import instagram from "../assets/instagram.jpg";
+import twitter from "../assets/twitter.jpg";
+import youtube from "../assets/youtube.jpg";
+
+const SocialIcons = ({ className = "" }) => {
+  return (
+    <div className={`social-icons ${className}`}>
+      <a href="#" className="social-icon">
+        <img src={instagram} alt="Instagram" />
+      </a>
+      <a href="#" className="social-icon">
+        <img src={twitter} alt="Twitter" />
+      </a>
+      <a href="#" className="social-icon">
+        <img src={facebook} alt="Facebook" />
+      </a>
+      <a href="#" className="social-icon">
+        <img src={youtube} alt="YouTube" />
+      </a>
+    </div>
+  );
+};
+
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobileComicsDropdownOpen, setIsMobileComicsDropdownOpen] =
+    useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleMobileComicsDropdown = () => {
+    setIsMobileComicsDropdownOpen(!isMobileComicsDropdownOpen);
+  };
+
   return (
     <header className="header">
       <div className="header-container">
-        {/* Left side - Brand logo */}
         <div className="brand-logo">
-          <img
-            src="path-to-purple-arrow-image.png"
-            alt="Arrow"
-            className="arrow-icon"
-          />
-          <span className="brand-text">INK UNDERGROUND COMICS</span>
+          <img src={logo} alt="Arrow" className="arrow-icon" />
+          <div className="brand-text">
+            <span>INK</span>
+            <span>UNDERGROUND</span>
+            <span>COMICS</span>
+          </div>
         </div>
 
-        {/* Center - Navigation menu */}
-        <nav className="nav-menu">
+        <nav className="nav-menu desktop-nav">
           <a href="#" className="nav-link">
             CHARACTERS
           </a>
           <a href="#" className="nav-link">
             NEW RELEASES
           </a>
-          <a href="#" className="nav-link">
-            COMICS
-          </a>
+          <div className="nav-link dropdown-container">
+            <button className="dropdown-toggle">COMICS</button>
+            <div className="dropdown-menu">
+              <a href="#" className="dropdown-item">
+                First Issue
+              </a>
+              <a href="#" className="dropdown-item">
+                New Releases
+              </a>
+              <a href="#" className="dropdown-item">
+                New Series
+              </a>
+            </div>
+          </div>
           <a href="#" className="nav-link">
             MERCH
           </a>
         </nav>
-        {/* Right side - Social media icons */}
-        <div className="social-icons">
-          <a href="#" className="social-icon">
-            <img src="path-to-instagram-icon.png" alt="Instagram" />
-          </a>
-          <a href="#" className="social-icon">
-            <img src="path-to-twitter-icon.png" alt="Twitter" />
-          </a>
-          <a href="#" className="social-icon">
-            <img src="path-to-facebook-icon.png" alt="Facebook" />
-          </a>
-          <a href="#" className="social-icon">
-            <img src="path-to-youtube-icon.png" alt="YouTube" />
-          </a>
-        </div>
+
+        <SocialIcons className="desktop-social" />
+
+        <button
+          className="hamburger-menu"
+          onClick={toggleMenu}
+          aria-label="Toggle navigation menu"
+        >
+          <span className="hamburger-line"></span>
+          <span className="hamburger-line"></span>
+          <span className="hamburger-line"></span>
+        </button>
       </div>
+
+      {isMenuOpen && (
+        <div className="mobile-menu">
+          <nav className="mobile-nav">
+            <a href="#" className="mobile-nav-link">
+              CHARACTERS
+            </a>
+            <a href="#" className="mobile-nav-link">
+              NEW RELEASES
+            </a>
+            <div className="mobile-dropdown-container">
+              <button
+                className="mobile-dropdown-toggle"
+                onClick={toggleMobileComicsDropdown}
+              >
+                COMICS
+              </button>
+              {isMobileComicsDropdownOpen && (
+                <div className="mobile-dropdown-menu">
+                  <a href="#" className="mobile-dropdown-item">
+                    First Issue
+                  </a>
+                  <a href="#" className="mobile-dropdown-item">
+                    New Releases
+                  </a>
+                  <a href="#" className="mobile-dropdown-item">
+                    New Series
+                  </a>
+                </div>
+              )}
+            </div>
+            <a href="#" className="mobile-nav-link">
+              MERCH
+            </a>
+          </nav>
+
+          <SocialIcons className="mobile-social-icons" />
+        </div>
+      )}
     </header>
   );
 };
